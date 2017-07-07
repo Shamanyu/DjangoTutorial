bee98e9418b36ae72879a8c74910e3284398bbb5
shamanyu <shubhamshamanyu@gmail.com>
2017-07-07 16:07:10 +0530

    Write test cases for Index details view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -40,6 +40,12 @@ class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
 
+	def get_queryset(self):
+		"""
+		Excludes any questions that aren't published yet.
+		"""
+		return Question.objects.filter(pub_date__lte=timezone.now())
+
 class ResultsView(generic.DetailView):
 	# # response = "You're looking at the results of question %s."
 	# # return HttpResponse(response % question_id)
